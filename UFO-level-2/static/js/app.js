62ba9895574b55d459001491360d1cb6b683fcd8
Randy Dettmer <randy.dettmer@gmail.com>
2020-05-07 18:09:19 -0400

    update part 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -21,63 +21,73 @@ var button = d3.selectAll("#filter-btn");
 // select the form //
 var form = d3.selectAll("#form");
 
+//test test
+/*
+var theParent = document.querySelector("#filter-btn");
+theParent.addEventListener("click", runEnter, false);
+*/
+
+
+/* - do not run -
+theParent.addEventListener("click", doSomething, false);
+
+function doSomething(e) {
+    if (e.target !== e.currentTarget){
+        var clickedItem = e.target.id;
+        alert("Hello" + clickedItem);
+    }
+    e.stopPropagation();
+}
+*/
+
+
 // create event handlers //
-button.on("click", runEnter);
-form.on("submit", runEnter);
+button.on("click", runEnter, false);
+form.on("submit", runEnter, false);
 
 // complete the event handler function for the form //
 function runEnter() {
 
     // prevent page from refreshing
-    //d3.event.preventDefault();
+    d3.event.preventDefault();
 
     // clear any prior data in table
     tbody.html("");
-
 
     // test for empty value
     if (d3.select("#datetime") !=="") {
         var inputElement = d3.select("#datetime");
         var inputValue = inputElement.property("value");
-
+        console.log(inputValue);
         // filter data on input element
         var filteredData = tableData.filter(data => data.datetime === inputValue);
-        //console.log(filteredData);
-    } else if (d3.select("#city") !=="") {
+        console.log(filteredData);
+    } 
+    else if (d3.select("#city") !=="") {
         var inputElement = d3.select("#city");
         var inputValue = inputElement.property("value");
-    
+        console.log(inputValue);
         // filter data on input element
         var filteredData = tableData.filter(data => data.city === inputValue);
-        //console.log(filteredData);
-    } else if (d3.select("#state") !=="") {
+        console.log(filteredData);
+    } 
+    else if (d3.select("#state") !=="") {
         var inputElement = d3.select("#state");
         var inputValue = inputElement.property("value");
-
+        console.log(inputValue);
         // filter data on input element
         var filteredData = tableData.filter(data => data.state === inputValue);
-        //console.log(filteredData);       
-    } else {
+        console.log(filteredData);       
+    } 
+    else {
         var inputElement = d3.select("#shape");
         var inputValue = inputElement.property("value");
-        
+        console.log(inputValue);
         // filter data on input element
         var filteredData = tableData.filter(data => data.shape === inputValue);
-        //console.log(filteredData);       
+        console.log(filteredData);       
     }
 
-    //select input element and get the raw HTML code
-    //var inputElement = d3.select("#datetime");
-
-    // get the value property of the input element
-    //var inputValue = inputElement.property("value");
-
-    //console.log(inputValue);
-    //console.log(tableData);
-    
-    // filter data on input element
-    //var filteredData = tableData.filter(date => date.datetime === inputValue);
-    //console.log(filteredData);
 
     // render the filtered data in the table
     filteredData.forEach(function(displayData) {
@@ -89,12 +99,4 @@ function runEnter() {
         });
     });
 };
-
-// create multiple search categories - add drop down
-
-// date/time - "1/1/2010" - completed above
-// city - "la mesa"
-// state - "ca"
-// country - "us"
-// shape - "triangle"
 
