76e28ea9acf47add92f7dbe8b5fa880926388863
Randy Dettmer <58636741+RADettmer@users.noreply.github.com>
2020-05-27 14:31:12 -0400

    update level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,10 +1,10 @@
-// app.js file - Randy Dettmer 2020/05/04 - level 2
+// app.js file - Randy Dettmer 2020/05/27 - level 2
 // assign the data from data.js to a descriptive variable
 var tableData = data;
 
 var tbody = d3.select("tbody");
 
-// use d3 to display data //
+// use d3 to display initial data //
 data.forEach(function(tableData) {
     //console.log(tableData);
     var row = tbody.append("tr");
@@ -22,19 +22,10 @@ var clearbutton = d3.select("#clear-btn");
 // select the form //
 var form = d3.select("#form");
 
-
 // create event handlers //
 button.on("click", runEnter, false);
 form.on("submit", runEnter, false);
 clearbutton.on("click", runClear, false);
-
-//test to accept all values - test outside function
-var datetime = d3.select("#datetime");
-var city = d3.select("#city");
-//console.log(city);
-var state = d3.select("#state");
-//var shape = d3.select("#shape");
-
 
 // complete the event handler function for the form //
 function runEnter() {
@@ -45,53 +36,37 @@ function runEnter() {
     // clear any prior data in table
     tbody.html("");
 
-    //test to accept all values - test inside function
-    //var datetime = d3.select("#datetime");
-    //var city = d3.select("#city");
-    console.log(city);
-    //var state = d3.select("#state");
-    //var shape = d3.select("#shape");
+    // accept all values
+    var datetime = d3.select("#datetime").property("value");
+    //console.log(datetime);
+    var city = d3.select("#city").property("value");
+    //console.log(city);
+    var state = d3.select("#state").property("value");
+    //console.log(state);
+    var shape = d3.select("#shape").property("value");
+    //console.log(shape);
 
 
-    // test for empty value - this.value????
-   if (datetime.length != 0) {
-        var inputElement = d3.select("#datetime");
-        //console.log(inputElement);
-        var inputValue = inputElement.property("value");
-        console.log(inputValue);
-        // filter data on input element
-        var filteredData = tableData.filter(data => data.datetime === inputValue);
-        console.log(filteredData);
-    } else if (city.length != 0) {
-        var inputElement = d3.select("#city");
-        console.log(inputElement);
-        var inputValue = inputElement.property("value");
-        console.log(inputValue);
-        // filter data on input element
-        var filteredData = tableData.filter(data => data.city === inputValue);
-        console.log(filteredData);
-    } else if (state.length != 0) {
-        var inputElement = d3.select("#state");
-        console.log(inputElement);
-        var inputValue = inputElement.property("value");
-        console.log(inputValue);
-        // filter data on input element
-        var filteredData = tableData.filter(data => data.state === inputValue);
-        console.log(filteredData);       
+    // test for value
+   if (datetime) {
+        var filteredData = tableData.filter(data => data.datetime === datetime);
+        //console.log(filteredData);
+    } else if (city) {
+        var filteredData = tableData.filter(data => data.city === city);
+        //console.log(filteredData);
+    } else if (state) {
+        var filteredData = tableData.filter(data => data.state === state);
+        //console.log(filteredData);       
     } else {
-        var inputElement = d3.select("#shape");
-        var inputValue = inputElement.property("value");
-        console.log(inputValue);
-        // filter data on input element
-        var filteredData = tableData.filter(data => data.shape === inputValue);
-        console.log(filteredData);       
+        var filteredData = tableData.filter(data => data.shape === shape);
+        //console.log(filteredData);       
     }
 
 
     // render the filtered data in the table
-    filteredData.forEach(function(displayData) {
+    filteredData.forEach((displayData) => {
         var row = tbody.append("tr");
-        Object.entries(displayData).forEach(function([key, value]) {
+        Object.entries(displayData).forEach(([key, value]) => {
             //console.log(key,value);
             var cell = row.append("td");
             cell.text(value);
@@ -99,11 +74,19 @@ function runEnter() {
     });
 };
 
-function runClear() {
-    let inputs = d3.selectAll(".form-control");
-  // prevent page from refreshing
-  d3.event.preventDefault();
 
-  // clear any prior data in table
-  inputs.html("");
-}
+var filterSearch = d3.selectAll(".form-control");
+
+// clears input fields and table
+function runClear() {
+    filters = {};
+
+    // resets every search filter
+    filterSearch._groups[0].forEach(entry => {
+        if (entry.value != 0) {
+            d3.select("#" + entry.id).node().value = "";
+        }
+    // clears table    
+    tbody.html("")
+    });
+};
